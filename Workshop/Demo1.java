573c21fbadbcdd8c52234e97408b5a37695e412b
Shivam Upadhyay <shivamu2108gmail.com>
2026-02-17 15:26:22 +0530

    program for finding demo1

---
+++ Workshop/Demo1.java
@@ -0,0 +1,10 @@
+import java.util.StringTokenizer;
+public class Demo1 {
+    static int addition(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int result = addition(10,20);
+        System.out.println(result);
+    }
+}
